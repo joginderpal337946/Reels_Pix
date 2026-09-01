862e530014f4a2f1fb99bcf5d7129587dae446f2
Joginder Pal <joginderpal337946@gmail.com>
2026-09-01 18:48:17 +0530

    home screen changes done

--- app/src/androidTest/java/com/dramzz/reels_pix/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dramzz/reels_pix/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.reels_pix", appContext.packageName)
+        assertEquals("com.dramzz.reels_pix", appContext.packageName)
     }
 }
